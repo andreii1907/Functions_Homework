f2ac6ce12f51ad035a83e109d1cdc84963afc065
andreii1907 <luchianandrei1024@yahoo.com>
2021-04-03 16:57:44 +0300

    part-2

--- Exercises_Part2/script.js
+++ Exercises_Part2/script.js
@@ -1,22 +1,23 @@
 // ------ Exercise 1 ------ //
 
-function convertDegrees(degrees){
-    let fromFahrenheit = celsius * 1.8 + 32;
-    return fromFahrenheit;
+function convertDegrees(degrees) {
+    if (degrees.slice(- 1) === 'F') {
+        let value = degrees.slice(0, degrees.length - 1);
+        let farenheit = (value - 32) * 5 / 9;
+        return farenheit;
+    } else if (degrees.slice(- 1) === 'C') {
+        let value = degrees.slice(0, degrees.length - 1);
+        let celsius = value * 1.8 + 32; 
+        return celsius;
+    } 
+      else {
+          console.log ('Trebuie sa adaugi tip de grade pe care vrei sa il convertesti la final ("C" ori "F")')
+      }
 }
 
-console.log(`25 de grade celsius sunt ${convertCelsius(25)} de grade fahrenheit`);
+console.log(convertDegrees('33C'));
 
-
-
-function convertFahrenheit(fahrenheit){
-    let celsius = (fahrenheit - 32) * 5 / 9;
-    return celsius;
-}
-
-console.log(`64 grade fahrenheit sunt  ${convertFahrenheit(64)} de grade fahrenheit`);
-
-// ------- Exercise 2 ------- // 
+// // ------- Exercise 2 ------- // 
 
 function getDistance(point1, point2) {
     const distance = (point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2;
@@ -27,10 +28,10 @@ console.log(getDistance({ x: 7, y: 4 }, { x: 13, y: 9 }));
 
 // ------- Exercise 3 ------- // 
 
-function getDistancesArray(pair1, pair2){
-    const distanceArrays = (pair1.x - pair2.x) ** 2 + (pair1.y - pair2.y) ** 2;
-    return Math.sqrt(distanceArrays); 
-}
+// function getDistancesArray(pair1, pair2) {
+//     const distanceArrays = (pair1.x - pair2.x) ** 2 + (pair1.y - pair2.y) ** 2;
+//     return Math.sqrt(distanceArrays); 
+// }
   
-console.log(getDistancesArray([[{ x: 2, y: 3 }, { x: 1, y: 4 }]
-    [{ x: -1, y: 2 }, { x: 2, y: -3 }]]));
+// console.log(getDistancesArray([[{ x: 2, y: 3 }, { x: 1, y: 4 }]
+//     [{ x: -1, y: 2 }, { x: 2, y: -3 }]]));
