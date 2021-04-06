7edde2da60725e132b6c4edd19a456e1a945a1fd
Luchian Andrei <luchianandrei1024@yahoo.com>
2021-04-06 16:36:02 +0300

    Part-5

--- Exercises_Part5/newscript.js
+++ Exercises_Part5/newscript.js
@@ -1,21 +1,56 @@
-// ------- Exercise with Chess Board ------- //
+// ------- Exercise with triangle ------- //
 
-function chessBoard(square) {
-    let board = '';
-
-    for (let i=1; i <= square; i++) {
-        for (let j=1; j <= square; i++){
-            if ((i+j) % 2 == 0) {
-                board += '';
-            } else {
-                board += '#';
-            }
+function triangle(num) {
+    
+    let line = ''
+    for (let i = 1; i <= num; i++) {
+        for (let j = 1; j <= i; j++){
+            line += '#'
         }
-        
-        board += "\n";
+        line += '\n';
     }
-
-    console.log(board);
+    return line;
 }
 
-console.log(chessBoard(16));
+console.log(triangle(5));
+
+// ------- Exercise with tree ------- //
+
+function christmasTree(branches) {
+    let threads = '';
+    for(i = 1; i <= branches; i++) {
+        threads = '';
+        for(j = i; j < branches; j++) {
+        threads += ' ';
+        }
+        for(k = 0; k < (i*2)-1; k++) {
+        threads += '*';
+        }
+    console.log(threads);
+    }
+    return threads;
+}
+
+christmasTree(5);
+
+// ------- Exercise with Chess Board ------- //
+
+function chessBoard(size) {
+
+    let board = "";
+    for (let i = 0; i < size; i++) {  
+        for (let j = 0; j < size; j++) {
+            if ((i + j) % 2 == 0){
+                board += " ";
+            } else {
+                board += "#";
+            }
+        }
+    board += "\n";
+    }
+    return board;
+}
+
+console.log(chessBoard(8));
+
+
